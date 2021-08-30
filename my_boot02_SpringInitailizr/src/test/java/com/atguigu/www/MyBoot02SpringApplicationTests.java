6caf8acaf9dbc18a42c1e4f48f4f9e6f201cfad5
liming <ccclane@foxmail.com>
2021-08-30 23:12:29 +0800

    update

---
+++ my_boot02_SpringInitailizr/src/test/java/com/atguigu/www/MyBoot02SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBoot02SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
