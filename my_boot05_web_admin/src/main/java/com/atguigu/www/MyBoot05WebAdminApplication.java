626e524a8d7f1823d9699c0dd514d1d2f00c90e3
liming <ccclane@foxmail.com>
2021-09-07 21:57:08 +0800

    update-P155-44

---
+++ my_boot05_web_admin/src/main/java/com/atguigu/www/MyBoot05WebAdminApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoot05WebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBoot05WebAdminApplication.class, args);
+    }
+
+}
---
+++ my_boot05_web_admin/src/test/java/com/atguigu/www/MyBoot05WebAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBoot05WebAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
